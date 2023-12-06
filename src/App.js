655ffd8be145588d289c6fe373ca9815f61193ab
Raviteja0405 <raviteja40025040@gmail.com>
2023-12-06 20:30:38 +0530

    intial reactJs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
+
+  //state
+  let a = 10;
+  let personName = "Ravi"
+  let student = {
+    sno:100,
+    name: "bhanu"
+  }
+
+  let skills = ['js', 'angular', 'react', 'node','java', 'Dotnet']
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-danger display-1 bg-warning'>Welcome to React</h1>
+      <h5 style={{backgroundColor: 'red',color:'white'}}>Inline CSS</h5>
+      <p>Value of a is {a}</p>
+      <p>Name of person is : {personName}</p>
+      <h2>Sno : {student.sno}</h2>
+      <h2>Name : {student.name}</h2>
+      {/* We can use for or some loops but they do not return anything, Here we need to return something */}
+      {
+        // skills.map(skill=><h2>{skill}</h2>) //you will get a warning
+        skills.map((skill,index)=> <h2 key = {index}>{skill}</h2>)
+      }
     </div>
   );
 }
