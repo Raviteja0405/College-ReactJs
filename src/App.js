3c4f5fe360d70109864b2957a56046ca0b108537
Raviteja0405 <raviteja40025040@gmail.com>
2023-12-06 20:40:52 +0530

    add app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css'
+import img1 from './images/pic-1.webp'
 
 function App() {
 
@@ -15,6 +16,9 @@ function App() {
   return (
     <div>
       <h1 className='text-danger display-1 bg-warning'>Welcome to React</h1>
+      <img src = "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?q=80&w=1948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt= "" />
+      {/* Adding images */}
+    <img src={img1} alt = ""/>
       <h5 style={{backgroundColor: 'red',color:'white'}}>Inline CSS</h5>
       <p>Value of a is {a}</p>
       <p>Name of person is : {personName}</p>
