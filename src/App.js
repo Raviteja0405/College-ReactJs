1ad960bc8d505b203c018cd78ca33d12d187fed4
Raviteja0405 <raviteja40025040@gmail.com>
2023-12-18 20:46:16 +0530

    added useState Hook

--- src/App.js
+++ src/App.js
@@ -38,10 +38,12 @@
 //   );
 // }
 
-import "./App.css";
-import Test1 from "./components/Test-nesting/test1/Test1";
-// import Test2 from "./components/test2/Test2"
-function App() {
+
+// Second Time 
+// import "./App.css";
+// import Test1 from "./components/Test-nesting/test1/Test1";
+// // import Test2 from "./components/test2/Test2"
+// function App() {
   //state
   // let emps = [
   //   { eno: 100, name: "ravi", age: 21, city: "hyd" },
@@ -59,60 +61,71 @@ function App() {
   // function testAgain(a){
   //   console.log("test again called and a is",a);
   // }
-  return (
-    <div>
-      <h1 className="text-info display-1 bg-light">Welcome to React</h1>
-      <div className="container">
-        {/* Table of emps */}
-        {/* <table className="table text-center">
-        <thead>
-          <tr>
-            <th>Eno</th>
-            <th>Name</th>
-            <th>Age</th>
-            <th>City</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* inside jsx if you want to write js, you need to write it in pair of braces  */}
-        {/* {
-            emps.map(empObj => <tr key= {empObj.eno}>
-              <td>{empObj.eno}</td>
-              <td>{empObj.name}</td>
-              <td>{empObj.age}</td>
-              <td>{empObj.city}</td>
-            </tr>)
-          }
-        </tbody>
-      </table> */}
-        {/* <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
-          {emps.map((empObj) => (
-            <div className="col" key={empObj.eno}>
-              <div className="card">
-                <div className="card-body">
-                  <h3>{empObj.eno}</h3>
-                  <p className="lead">{empObj.name}</p>
-                  <p className="lead">{empObj.age}</p>
-                  <p className="lead">{empObj.city}</p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div> */}
-        {/* Event Handling */}
-        {/* <button className="btn btn-success" onClick={test}>Click on me</button> un parameterised */}
-        {/* <button className="btn btn-warning" onClick={()=>testAgain(100)}>Click on me</button> parameterised, we should'nt call it directly */}
-        {/* we use "on" before the event, and inside {} these we should have a request handler function which is declared bofore return word i.e in states...(ig)*/}
-        {/* test is the name of the function we should just give name not ()*/}
-        {/* Component nesting */}
-        {/* first we are making test1 and test2 as children of root element, to make a children we import */}
-        <Test1 />{" "}
-        {/* we can also write opening and closing tags  if we want something inside it*/}
-        {/* <Test2></Test2> */}
-        {/* Now making test1 as root chlld and test2 as child of test1 */}
-      </div>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <h1 className="text-info display-1 bg-light">Welcome to React</h1>
+  //     <div className="container">
+      //   {/* Table of emps */}
+      //   {/* <table className="table text-center">
+      //   <thead>
+      //     <tr>
+      //       <th>Eno</th>
+      //       <th>Name</th>
+      //       <th>Age</th>
+      //       <th>City</th>
+      //     </tr>
+      //   </thead>
+      //   <tbody>
+      //     {/* inside jsx if you want to write js, you need to write it in pair of braces  */}
+      //   {/* {
+      //       emps.map(empObj => <tr key= {empObj.eno}>
+      //         <td>{empObj.eno}</td>
+      //         <td>{empObj.name}</td>
+      //         <td>{empObj.age}</td>
+      //         <td>{empObj.city}</td>
+      //       </tr>)
+      //     }
+      //   </tbody>
+      // </table> */}
+      //   {/* <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+      //     {emps.map((empObj) => (
+      //       <div className="col" key={empObj.eno}>
+      //         <div className="card">
+      //           <div className="card-body">
+      //             <h3>{empObj.eno}</h3>
+      //             <p className="lead">{empObj.name}</p>
+      //             <p className="lead">{empObj.age}</p>
+      //             <p className="lead">{empObj.city}</p>
+      //           </div>
+      //         </div>
+      //       </div>
+      //     ))}
+      //   </div> */}
+      //   {/* Event Handling */}
+      //   {/* <button className="btn btn-success" onClick={test}>Click on me</button> un parameterised */}
+      //   {/* <button className="btn btn-warning" onClick={()=>testAgain(100)}>Click on me</button> parameterised, we should'nt call it directly */}
+      //   {/* we use "on" before the event, and inside {} these we should have a request handler function which is declared bofore return word i.e in states...(ig)*/}
+      //   {/* test is the name of the function we should just give name not ()*/}
+      //   {/* Component nesting */}
+      //   {/* first we are making test1 and test2 as children of root element, to make a children we import */}
+      //   // <Test1 />{" "}
+      //   {/* we can also write opening and closing tags  if we want something inside it*/}
+      //   {/* <Test2></Test2> */}
+      //   {/* Now making test1 as root chlld and test2 as child of test1 */}
+//       </div>
+//     </div>
+//   );
+// }
+
+
+// Third time
+import "./App.css";
+import StateDemo from "./components/states/state-demo/StateDemo"
+function App(){
+  return <div className="app">
+    {/* nest stateDemo */}
+    <StateDemo/>
+  </div>
 }
 
 export default App;
