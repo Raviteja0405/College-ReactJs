c56ec084eaa50bcc6f42c2e0a15ec770dc56fc8d
Raviteja0405 <raviteja40025040@gmail.com>
2023-12-18 20:18:28 +0530

    nesting of components

---
+++ src/components/test2/Test2.js
@@ -0,0 +1,14 @@
+// Create test2 component
+
+import './Test2.css'
+
+function Test2(){
+    //state
+    return(
+        <div className="test2-div">
+            <h1>Test2 Component</h1>
+        </div>
+    )
+}
+
+export default Test2;
