ca1abd75a8889a11e8df3ac793550657098fefa1
Raviteja0405 <raviteja40025040@gmail.com>
2023-12-19 21:15:31 +0530

    new version of test2

--- src/components/Test-nesting/test2/Test2.js
+++ src/components/Test-nesting/test2/Test2.js
@@ -1,12 +1,22 @@
-// Create test2 component
+// // Create test2 component
 
-import './Test2.css'
+// import './Test2.css'
 
-function Test2(){
-    //state
-    return(
-        <div className="test2-div">
-            <h1>Test2 Component</h1>
+// function Test2(){
+//     //state
+//     return(
+//         <div className="test2-div">
+//             <h1>Test2 Component</h1>
+//         </div>
+//     )
+// }
+
+
+function Test2(props){ //props is a single object // { x : 100}
+    return (
+        <div>
+            <h1>Child</h1>
+            <h2>{props.x}</h2>
         </div>
     )
 }
