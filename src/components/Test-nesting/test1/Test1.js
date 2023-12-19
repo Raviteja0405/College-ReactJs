e1347383e5428add4c52d3ff7e674ecd4a7ad4e1
Raviteja0405 <raviteja40025040@gmail.com>
2023-12-19 21:15:19 +0530

    new version of test1

--- src/components/Test-nesting/test1/Test1.js
+++ src/components/Test-nesting/test1/Test1.js
@@ -1,16 +1,34 @@
-// Create Test1 component
+// // Create Test1 component
 
-//connect css file to component
-import "./Test1.css";
+// //connect css file to component
+// import "./Test1.css";
+// import Test2 from "../test2/Test2";
+// function Test1() {
+//   //state
+
+//   return (
+//     <div className="test1-div">
+//       <h1>Test1 component</h1>
+//       {/* Nest test2 */}
+//       <Test2 />
+//     </div>
+//   );
+// }
+
 import Test2 from "../test2/Test2";
+import { useState } from "react";
+
 function Test1() {
-  //state
+  // state
+  let [counter, setCounter] = useState(100);
+  let [user, setUser] = useState({ id: 1, name: "ravi" });
+  // we want to convert state of parent to child(transfering)
 
   return (
-    <div className="test1-div">
-      <h1>Test1 component</h1>
+    <div className="bg-warning">
+      <h1>Parent</h1>
       {/* Nest test2 */}
-      <Test2 />
+      <Test2 x={counter} /> {/* x is the attribute*/}
     </div>
   );
 }
